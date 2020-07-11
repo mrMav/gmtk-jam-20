4f884dd28b7fc88eaa0a0ea4378c936136712324
Torres-Git <luismigueltorres@outlook.pt>
2020-07-11 20:58:11 +0100

    Inimigo Book

--- Game/scripts/EnemyFollow.gd
+++ Game/scripts/EnemyFollow.gd
@@ -1,14 +1,16 @@
 extends KinematicBody2D
 
 export(NodePath) var player_target = null
+onready var animation = get_node("AnimatedSprite")
 
 var target
 
-const speed = 400
+const speed = 200
 const detect_dist = 16*15;
 const drag = 0.98
 
-var velocity = Vector2.ZERO
+var velocity : Vector2 = Vector2.ZERO
+
 
 func _ready():
 	target = get_node(player_target)
@@ -16,14 +18,23 @@ func _ready():
 
 func _physics_process(delta):
 	
+	
 	var space_state = get_world_2d().direct_space_state
 	var result = space_state.intersect_ray(global_position, target.position, [self])
 	
 	if(result):		
 		if(result.collider.name == target.name):
+			animation.play("Walk")
 			var dist = (target.position - position)			
 			if( dist.length() <= detect_dist):		
 				velocity += dist.normalized() * speed * delta	
+		else:
+			animation.play("Idle")
 			
 	velocity = move_and_slide(velocity)			
 	velocity *= drag
+	
+	if(velocity.x > 0):
+		animation.flip_h = true
+	else:
+		animation.flip_h = false
