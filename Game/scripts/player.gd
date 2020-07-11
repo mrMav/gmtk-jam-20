054b1a521fb01803a7ab5746d239ad21331899fd
mrmav <mrmav@outlook.pt>
2020-07-11 11:42:25 +0100

    added spell properties; spells have timers to selk kill; particles die only on last particle death

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -15,12 +15,12 @@ var velocity : Vector2 = Vector2.ZERO
 onready var cursor = get_node(cursorPath)
 onready var spell_container = get_node(spellContainerPath)
 
-var projectile_spell = preload("res://scenes/spell.tscn")
+var projectile_spell = preload("res://scenes/projectile_spell.tscn")
 
 func _process(delta):	
 	
 	if(Input.is_action_just_pressed("action_1")):
-		_spawn_spell()
+		_spawn_projectile_spell()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -42,25 +42,66 @@ func _physics_process(delta):
 		
 	velocity *= drag
 	
-func _spawn_spell():
+func _spawn_projectile_spell():
 	
 	# from the list of spells
 	# grab one and spawn with the apropriate type
 	spells.shuffle()  # make actual random
-	var spell_resource : ProjectileSpell = spells[0];
+	var spell_resource : ProjectileSpell = spells[0]
 	
-	var s = projectile_spell.instance()
+	# instanciate the number of required spells
+	var spells = []
+	for i in range(spell_resource.number_of_projectiles):
+		
+		# create the instance
+		spells.push_back(projectile_spell.instance())
+		
+		# set the properties
+		# set the image
+		var sprite : Sprite = spells[i].get_node("Visuals/Sprite")
+		sprite.texture = spell_resource.profile
+		
+		var body = spells[i].get_node("spell_body")
+		
+		# set the direction (take into account spread)
+		var direction : Vector2 = -(position - cursor.position)
+		var half_n_projectiles = spell_resource.number_of_projectiles / 2.0 - 0.5
+		direction = direction.rotated(deg2rad((spell_resource.spread * i)-(half_n_projectiles * spell_resource.spread)))
+		body.direction = direction.normalized()
+		
+		# set position to the current player position
+		body.position = position
+		
+		# set the speed that this spell travels at
+		body.acceleration = spell_resource.acceleration
+		
+		# set the self rotation of the spell
+		body.rotation_speed = spell_resource.rotation_speed
+		
+		# set damage
+		body.damage = spell_resource.damage
+				
+		body.drag = spell_resource.drag
+				
+		body.time_to_kill = spell_resource.time_to_kill
+		
+		spell_container.add_child(spells[i], true)
 	
-	# set the image
-	var sprite : Sprite = s.get_node("Visuals/Sprite")
-	sprite.texture = spell_resource.profile
+#	var s = projectile_spell.instance()
 	
-	var direction : Vector2 = position - cursor.position
-	s.speed = spell_resource.velocity
-	s.direction = -direction.normalized()
-	s.position = position
 	
-	spell_container.add_child(s, true)
+#	var spell_body = s.get_node("spell_body")
+	
+	# set all the projectile variables from created resource	
+#	spell_body.speed = spell_resource.velocity
+#	spell_body.direction = -direction.normalized()
+#	spell_body.position = position
+#	spell_body.damage = spell_resource.damage
+#	spell_body.number_of_projectiles = spell_resource.number_of_projectiles
+#	spell_body.spread = spell_resource.spread
+#	spell_body.rotation_speed = spell_resource.rotation_speed
+	
+#	spell_container.add_child(s, true)
 	
 	
 	
---
+++ Game/scripts/projectile_spell_behaviour.gd
@@ -0,0 +1,75 @@
+extends Area2D
+
+var acceleration
+var direction
+var damage
+var rotation_speed
+var drag
+var time_to_kill
+
+var velocity : Vector2 = Vector2.ZERO
+
+var spell_killed = false
+
+onready var visuals = get_node("../Visuals")
+onready var spell_rem_timer = get_node("../spell_remove_timer")
+onready var particle_rem_timer = get_node("../particle_remove_timer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	velocity = direction * acceleration
+	
+	if(time_to_kill > 0):
+		spell_rem_timer.start(time_to_kill)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _physics_process(delta):
+	
+	#if(not get_node("CollisionShape2D").disabled):
+	position += velocity	
+	velocity *= drag	
+	
+	# manually set the visuals position
+	# because it is not a child
+	visuals.position = position
+	# rotate
+	visuals.rotation += rotation_speed * delta
+	
+	# check for death
+	if(time_to_kill > 0):
+		if(spell_rem_timer.time_left == 0):			
+			kill_spell()
+	
+	# check for particle death
+	if(get_node("CollisionShape2D").disabled):
+		if(particle_rem_timer.time_left == 0):		
+			kill_particles()
+	
+
+func kill_spell():
+	if(not spell_killed):
+		get_node("CollisionShape2D").disabled = true
+		visuals.get_node("Sprite").queue_free()
+		visuals.get_node("Particles2D").emitting = false
+		particle_rem_timer.start(visuals.get_node("Particles2D").lifetime)
+		spell_killed = true
+		print("killed spell")
+
+func kill_particles():
+	get_node("../").queue_free()
+	
+	print("killed spell master node")
+
+func _on_Area2D_body_entered(body):
+	
+	if(body.name != "Player"):
+		kill_spell()
+		
